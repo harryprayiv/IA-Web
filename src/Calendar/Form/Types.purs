bc39f7a75108663c7e8a0012385a8e3cd7860ca0
harryprayiv <harryprayiv@gmail.com>
2024-01-20 10:01:36 -0500

    halogen real world not compiling

--- src/Calendar/Form/Types.purs
+++
@@ -1,7 +0,0 @@
-module Types where
-
-
-import Data.Date (Date)
-import Data.Map as Map
-
-type Availability = Map.Map Date Boolean
--- src/Calendar/Utils.purs
+++
@@ -1,9 +0,0 @@
-module Utils where
-
-import Prelude
-
-import Halogen.HTML as HH
-import Halogen.HTML.Properties as HP
-
-css ∷ ∀ r i. String → HH.IProp (class ∷ String | r) i
-css = HP.class_ <<< HH.ClassName
